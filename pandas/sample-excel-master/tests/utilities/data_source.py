dadd2dda49a24d29fd03664408cbd9ab0beb0e9a
shashank-bhojappa <65142056+shashank-bhojappa@users.noreply.github.com>
2021-07-01 16:28:47 +0530

    Delete pandas directory

--- pandas/sample-excel-master/tests/utilities/data_source.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-
-class DataSource:
-
-    @classmethod
-    def data_path(cls, book_name) -> Path:
-        return Path(__file__).parent.parent.parent.joinpath(book_name)
